04a97e58ad38adefdef3f58263ef4da6eaa47abf
buhairong <22858111@qq.com>
2023-04-29 11:39:33 +0800

    feat: config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,11 +3,16 @@ require('@rushstack/eslint-patch/modern-module-resolution')
 
 module.exports = {
   root: true,
-  'extends': [
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
     '@vue/eslint-config-typescript',
-    '@vue/eslint-config-prettier/skip-formatting'
+    '@vue/eslint-config-prettier'
   ],
   parserOptions: {
     ecmaVersion: 'latest'
